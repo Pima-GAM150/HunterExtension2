a67147d7c396f5207a84e1ae921d97921d09bf16
Hunter <hbova@mail.pima.edu>
2019-04-04 15:50:41 -0700

    Got an EditorWindow to appear
    Project prepped and ready to go

---
+++ Assets/ObjectResizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ObjectResizer : EditorWindow
+{
+    [MenuItem("Window/Object Resizer")]
+    static void Init()
+    {
+        ObjectResizer window = (ObjectResizer)EditorWindow.GetWindow(typeof(ObjectResizer));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+
+    }
+}
